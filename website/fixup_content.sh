c52d40cee2247a8030ced779f8b53cdc010e384c
Moritz Bunkus <mo@bunkus.online>
2023-04-05 19:51:24 +0200

    re-add page titles during processing
    Otherwise Jekyll won't convert them to HTML & leave them as Markdown
    instead.

--- website/fixup_content.sh
+++ website/fixup_content.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
-exec sed -E \
+title="$1"
+
+(
+  if test -n "$title"; then
+    printf -- '---\ntitle: %s\n---\n' "$title"
+  fi
+  cat
+) | sed -E \
      -e 's!https?://(www.)?matroska.org/!/!g' \
      -e 's!^Table:!\nTable:!'
